fbbbbeffe548a3187cf68f479b5a5f28828e03b8
Stefan <dinca.stefan960@gmail.com>
2024-04-21 22:13:54 +0300

    v5.0.0b

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
+const sass = require("sass");
 const erori = require("./erori.json");
+const galerie = require("./resurse/json/galerie.json");
 const fs = require("fs"); 
+const sharp = require("sharp");
 app = express();
 console.log("Folder proiect", __dirname); 
 console.log("Cale fisier", __filename);
@@ -11,7 +14,7 @@ console.log("Director de lucru", process.cwd());
 // este calea absoluta a folderului in care se afla fisierul js rulat de node,
 // iar process.cwd() este calea absoluta a folderului din care am folosit comanda node
 
-const vect_foldere = ["temp"];
+const vect_foldere = ["temp", "backup"];
 vect_foldere.forEach((folder) => {
   const folderPath = path.join(__dirname, folder);
 
@@ -25,6 +28,9 @@ vect_foldere.forEach((folder) => {
 
 const obGlobal = {
   obErori: null,
+  folderScss: path.join(__dirname, "resurse/scss"),
+  folderCss: path.join(__dirname, "resurse/css"),
+  folderBackup: path.join(__dirname, "backup"),
 };
 
 function initErori() {
@@ -73,6 +79,69 @@ function afisareEroare(res, identificator, titlu, text, imagine) {
   });
 }
 
+function renderImagesBySeason(res) {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  let anotimp;
+
+  if (currentMonth >= 3 && currentMonth <= 5) {
+    anotimp = "primavara";
+  } else if (currentMonth >= 6 && currentMonth <= 8) {
+    anotimp = "vara";
+  } else if (currentMonth >= 9 && currentMonth <= 11) {
+    anotimp = "toamna";
+  } else {
+    anotimp = "iarna";
+  }
+
+  const filteredImages = galerie.imagini.filter(
+    (image) => image.anotimp === anotimp
+  );
+
+  return filteredImages;
+}
+
+async function resizeImage(inputPath, outputPath, width, height) {
+  try {
+    await sharp(inputPath)
+      .resize({ width: width, height: height })
+      .toFile(outputPath);
+  } catch (error) {
+    console.error("Eroare la redimensionarea imaginii", error);
+  }
+}
+async function generateSmallerImages(galerie, galeriePath) {
+  const smallWidth = 650;
+  const smallHeight = 650;
+  const mediumWidth = 700;
+  const mediumHeight = 700;
+  const bigWidth = 750;
+  const bigHeight = 750;
+
+  for (const imagine of galerie) {
+    const inputPath = galeriePath + "/" + imagine.cale_fisier;
+
+    await resizeImage(
+      inputPath,
+      galeriePath + "/small/" + imagine.cale_fisier,
+      smallWidth,
+      smallHeight
+    );
+    await resizeImage(
+      inputPath,
+      galeriePath + "/medium/" + imagine.cale_fisier,
+      mediumWidth,
+      mediumHeight
+    );
+    await resizeImage(
+      inputPath,
+      galeriePath + "/big/" + imagine.cale_fisier,
+      bigWidth,
+      bigHeight
+    );
+  }
+}
+
 app.set("view engine", "ejs");
 
 app.use("/resurse", express.static(path.join(__dirname + "/resurse")));
@@ -88,8 +157,17 @@ app.get(["/", "/home", "/index"], function (req, res) {
 });
 
 app.get("/despre", function (req, res) {
+    generateSmallerImages(galerie.imagini, galerie.cale_galerie);
     const userIp = req.ip;
-    res.render("pagini/despre", { userIp: userIp });
+    const galerie_filtrata = renderImagesBySeason(res);
+    const galerie_path = galerie.cale_galerie;
+    console.log("IMAGINI", galerie);
+    console.log("CALE", galerie_path, "\n\n\n\n");
+    res.render("pagini/despre", {
+      userIp: userIp,
+      galerie: galerie_filtrata,
+      galerie_path: galerie_path,
+    });
 });
 
 app.use("/resurse", function (req, res, next) {
@@ -144,9 +222,50 @@ app.get("/*", function (req, res) {
   });
 });
 
+function compileazaScss(caleScss, caleCss) {
+  console.log("cale:", caleCss);
+  if (!caleCss) {
+    let numeFisExt = path.basename(caleScss);
+    let numeFis = numeFisExt.split(".")[0];
+    caleCss = numeFis + ".css";
+  }
 
+  if (!path.isAbsolute(caleScss))
+    caleScss = path.join(obGlobal.folderScss, caleScss);
+  if (!path.isAbsolute(caleCss))
+    caleCss = path.join(obGlobal.folderCss, caleCss);
 
+  let caleBackup = path.join(obGlobal.folderBackup, "resurse/css");
+  if (!fs.existsSync(caleBackup)) {
+    fs.mkdirSync(caleBackup, { recursive: true });
+  }
 
+  let numeFisCss = path.basename(caleCss);
+  if (fs.existsSync(caleCss)) {
+    fs.copyFileSync(
+      caleCss,
+      path.join(obGlobal.folderBackup, "resurse/css", numeFisCss)
+    );
+  }
+  rez = sass.compile(caleScss, { sourceMap: true });
+  fs.writeFileSync(caleCss, rez.css);
+}
+vFisiere = fs.readdirSync(obGlobal.folderScss);
+for (let numeFis of vFisiere) {
+  if (path.extname(numeFis) == ".scss") {
+    compileazaScss(numeFis);
+  }
+}
+
+fs.watch(obGlobal.folderScss, function (eveniment, numeFis) {
+  console.log(eveniment, numeFis);
+  if (eveniment == "change" || eveniment == "rename") {
+    let caleCompleta = path.join(obGlobal.folderScss, numeFis);
+    if (fs.existsSync(caleCompleta)) {
+      compileazaScss(caleCompleta);
+    }
+  }
+});
 
 app.listen(8080);
 console.log("Serverul a pornit");
