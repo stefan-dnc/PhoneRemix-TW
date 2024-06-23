8505cc232661871a0e776e62a646e18493d6af65
Stefan <dinca.stefan960@gmail.com>
2024-06-23 16:00:31 +0300

    v8.1.0

--- index.js
+++ index.js
@@ -67,53 +67,54 @@ const obGlobal = {
   folderBackup: path.join(__dirname, "backup"),
 };
 
-app.get("/produse", function (req, res) {
-  console.log(req.query);
-  var conditieQuery = "";
-  if (req.query.tip) {
-    conditieQuery = ` where brand='${req.query.tip}'`;
-  }
-  client.query(
-    "select * from unnest(enum_range(null::categorie_brand))",
-    function (err, rezOptiuni) {
-      client.query(
-        `select * from produse ${conditieQuery}`,
-        function (err, rez) {
-          if (err) {
-            console.log(err);
-            afisareEroare(res, 2);
-          } else {
-            res.render("pagini/produse", {
-              produse: rez.rows,
-              optiuni: res.optiuni,
-            });
-          }
+app.post("/login", function (req, res) {
+  /*TO DO
+        testam daca a confirmat mailul
+    */
+  var username;
+  var formular = new formidable.IncomingForm();
+
+  formular.parse(req, function (err, campuriText, campuriFisier) {
+    var parametriCallback = {
+      req: req,
+      res: res,
+      parola: campuriText.parola[0],
+    };
+
+    Utilizator.getUtilizDupaUsername(
+      campuriText.username[0],
+      parametriCallback,
+      function (u, obparam, eroare) {
+        //proceseazaUtiliz
+        let parolaCriptata = Utilizator.criptareParola(obparam.parola);
+        if (u.parola == parolaCriptata && u.confirmat_mail == true) {
+          u.poza = u.poza
+            ? path.join("poze_uploadate", u.username, u.poza)
+            : "";
+          obparam.req.session.utilizator = u;
+          obparam.req.session.mesajLogin = "Bravo! Te-ai logat!";
+          console.log("Logare cu succes");
+          obparam.res.redirect("/index");
+        } else {
+          console.log("Eroare logare");
+          obparam.req.session.mesajLogin =
+            "Date logare incorecte sau nu a fost confirmat mailul!";
+          obparam.res.redirect("/index");
         }
-      );
-    }
-  );
+      }
+    );
+  });
 });
 
-app.get("/produs/:id", function (req, res) {
-  client.query(
-    "select * from unnest(enum_range(null::categorie_brand))",
-    function (err, rezOptiuni) {
-      client.query(
-        `select * from produse where id=${req.params.id}`,
-        function (err, rez) {
-          if (err) {
-            console.log(err);
-            afisareEroare(res, 2);
-          } else {
-            res.render("pagini/produs", {
-              prod: rez.rows[0],
-              optiuni: rezOptiuni.rows,
-            });
-          }
-        }
-      );
-    }
-  );
+app.use("/*", function (req, res, next) {
+  res.locals.optiuni = obGlobal.optiuni;
+  res.locals.Drepturi = Drepturi;
+  if (req.session.utilizator) {
+    req.utilizator = res.locals.utilizator = new Utilizator(
+      req.session.utilizator
+    );
+  }
+  next();
 });
 
 function initErori() {
@@ -234,125 +235,6 @@ async function generateSmallerImages(galerie, galeriePath) {
   }
 }
 
-app.set("view engine", "ejs");
-
-app.use("/resurse", express.static(path.join(__dirname + "/resurse")));
-app.use("/node_modules", express.static(path.join(__dirname + "/node_modules")));
-app.use("/poze_uploadate", express.static(path.join(__dirname + "/poze_uploadate")));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "frame-src 'self' https://www.youtube.com https://www.google.com ;"
-  );
-
-  client.query(
-    "select * from unnest(enum_range(null::categorie_brand))",
-    function (err, rezOptiuni) {
-      if (err) {
-        return next(err);
-      }
-      res.locals.optiuni = rezOptiuni.rows;
-      next();
-    }
-  );
-});
-
-app.get(["/", "/home", "/index"], function (req, res) {
-  client.query(
-    "select * from unnest(enum_range(null::categorie_brand))",
-    function (err, rezOptiuni) {
-      generateSmallerImages(galerie.imagini, galerie.cale_galerie);
-      const galerie_carousel = galerie.imagini;
-      const galerie_path = galerie.cale_galerie;
-      console.log("IMAGINI_CAROUSEL", galerie_carousel);
-      console.log("IMAGINI_NORMAL", galerie);
-      console.log("CALE", galerie_path, "\n\n\n\n");
-      res.render("pagini/index", {
-        galerie_path: galerie_path,
-        galerie_carousel: galerie_carousel,
-        optiuni: rezOptiuni.rows,
-      });
-    }
-  );
-});
-
-app.get("/despre", function (req, res) {
-  client.query(
-    "select * from unnest(enum_range(null::categorie_brand))",
-    function (err, rezOptiuni) {
-      generateSmallerImages(galerie.imagini, galerie.cale_galerie);
-      const userIp = req.ip;
-      const galerie_filtrata = renderImagesBySeason(res);
-      const galerie_path = galerie.cale_galerie;
-      console.log("IMAGINI", galerie);
-      console.log("CALE", galerie_path, "\n\n\n\n");
-      res.render("pagini/despre", {
-        userIp: userIp,
-        galerie: galerie_filtrata,
-        galerie_path: galerie_path,
-        optiuni: rezOptiuni.rows,
-      });
-    }
-  );
-});
-
-app.use("/resurse", function (req, res, next) {
-  const resourcePath = path.join(__dirname, "resurse", req.url);
-
-  if (fs.existsSync(resourcePath) && fs.lstatSync(resourcePath).isDirectory()) {
-    const indexPath = path.join(resourcePath, "index.html");
-    if (fs.existsSync(indexPath)) {
-      next();
-    } else {
-      afisareEroare(res, 403);
-    }
-  } else {
-    next();
-  }
-});
-
-app.get("/favicon.ico", function (req, res) {
-  const faviconPath = path.join(__dirname, "/resurse/favicon/favicon.ico");
-  res.sendFile(faviconPath);
-});
-
-app.use((req, res, next) => {
-  if (req.url.endsWith(".ejs")) {
-    afisareEroare(res, 400);
-  } else {
-    next();
-  }
-});
-
-app.get("/*", function (req, res) {
-  console.log(req.url);
-  res.render("pagini" + req.url, function (err, rezHtml) {
-    if (err) {
-      if (err.message.indexOf("Failed to lookup view") !== -1) {
-        const errorCode = parseInt(req.url.split("/")[1]);
-        const { obErori } = obGlobal;
-        const eroareData =
-          obErori.info_erori.find(
-            (eroare) => eroare.identificator === errorCode
-          ) || obErori.eroare_default;
-
-        afisareEroare(
-          res,
-          errorCode,
-          eroareData.titlu,
-          eroareData.text,
-          eroareData.imagine
-        );
-      } else {
-        throw err;
-      }
-    }
-    //console.log(rezHtml);
-    //console.log("Eroare: ", err);
-    res.send(rezHtml + "");
-  });
-});
-
 app.post("/inregistrare", function (req, res) {
   var username;
   var poza;
@@ -374,7 +256,7 @@ app.post("/inregistrare", function (req, res) {
 
       utilizNou.parola = campuriText.parola[0];
       utilizNou.culoare_chat = campuriText.culoare_chat[0];
-      utilizNou.poza = poza[0];
+      utilizNou.poza = poza;
       Utilizator.getUtilizDupaUsername(
         campuriText.username[0],
         {},
@@ -395,7 +277,8 @@ app.post("/inregistrare", function (req, res) {
                   raspuns: "Inregistrare cu succes!",
                   optiuni: rezOptiuni.rows,
                 });
-            });
+              }
+            );
           } else
             res.render("pagini/inregistrare", { err: "Eroare: " + eroare });
         }
@@ -475,6 +358,62 @@ for (let numeFis of vFisiere) {
   }
 }
 
+app.post("/profil", function (req, res) {
+  console.log("profil");
+  if (!req.session.utilizator) {
+    afisareEroare(res, 403);
+    return;
+  }
+  var formular = new formidable.IncomingForm();
+
+  formular.parse(req, function (err, campuriText, campuriFile) {
+    var parolaCriptata = Utilizator.criptareParola(campuriText.parola[0]);
+
+    AccesBD.getInstanta().updateParametrizat(
+      {
+        tabel: "utilizatori",
+        campuri: ["nume", "prenume", "email", "culoare_chat"],
+        valori: [
+          `${campuriText.nume[0]}`,
+          `${campuriText.prenume[0]}`,
+          `${campuriText.email[0]}`,
+          `${campuriText.culoare_chat[0]}`,
+        ],
+        conditiiAnd: [
+          `parola='${parolaCriptata}'`,
+          `username='${campuriText.username[0]}'`,
+        ],
+      },
+      function (err, rez) {
+        if (err) {
+          console.log(err);
+          afisareEroare(res, 2);
+          return;
+        }
+        console.log(rez.rowCount);
+        if (rez.rowCount == 0) {
+          res.render("pagini/profil", {
+            mesaj: "Update-ul nu s-a realizat. Verificati parola introdusa.",
+          });
+          return;
+        } else {
+          //actualizare sesiune
+          console.log("ceva");
+          req.session.utilizator.nume = campuriText.nume[0];
+          req.session.utilizator.prenume = campuriText.prenume[0];
+          req.session.utilizator.email = campuriText.email[0];
+          req.session.utilizator.culoare_chat = campuriText.culoare_chat[0];
+          res.locals.utilizator = req.session.utilizator;
+        }
+
+        res.render("pagini/profil", {
+          mesaj: "Update-ul s-a realizat cu succes.",
+        });
+      }
+    );
+  });
+});
+
 fs.watch(obGlobal.folderScss, function (eveniment, numeFis) {
   console.log(eveniment, numeFis);
   if (eveniment == "change" || eveniment == "rename") {
@@ -483,6 +422,227 @@ fs.watch(obGlobal.folderScss, function (eveniment, numeFis) {
       compileazaScss(caleCompleta);
     }
   }
+});
+
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(path.join(__dirname + "/resurse")));
+app.use("/node_modules", express.static(path.join(__dirname + "/node_modules")));
+app.use("/poze_uploadate", express.static(path.join(__dirname + "/poze_uploadate")));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "frame-src 'self' https://www.youtube.com https://www.google.com ;"
+  );
+
+  client.query(
+    "select * from unnest(enum_range(null::categorie_brand))",
+    function (err, rezOptiuni) {
+      if (err) {
+        return next(err);
+      }
+      res.locals.optiuni = rezOptiuni.rows;
+      next();
+    }
+  );
+});
+
+app.use("/resurse", function (req, res, next) {
+  const resourcePath = path.join(__dirname, "resurse", req.url);
+
+  if (fs.existsSync(resourcePath) && fs.lstatSync(resourcePath).isDirectory()) {
+    const indexPath = path.join(resourcePath, "index.html");
+    if (fs.existsSync(indexPath)) {
+      next();
+    } else {
+      afisareEroare(res, 403);
+    }
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.url.endsWith(".ejs")) {
+    afisareEroare(res, 400);
+  } else {
+    next();
+  }
+});
+
+client.query(
+  "select * from unnest(enum_range(null::tipuri_produse))",
+  function (err, rezCategorie) {
+    if (err) {
+      console.log(err);
+    } else {
+      obGlobal.optiuni = rezCategorie.rows;
+    }
+  }
+);
+
+//http://${Utilizator.numeDomeniu}/cod/${utiliz.username}/${token}
+app.get("/cod/:username/:token",function(req,res){
+    /*TO DO parametriCallback: cu proprietatile: request (req) si token (luat din parametrii cererii)
+        setat parametriCerere pentru a verifica daca tokenul corespunde userului
+    */
+    console.log(req.params);
+    
+    try {
+        var parametriCallback ={
+            req:req,
+            token:req.params.token
+        }
+        Utilizator.getUtilizDupaUsername(req.params.username,parametriCallback ,function(u,obparam){
+            let parametriCerere = {
+                tabel:"utilizatori",
+                campuri:{"confirmat_mail":true},
+                conditiiAnd:[
+                    [ `id='${u.id}'`]
+                ]
+            }
+            AccesBD.getInstanta().update(
+                parametriCerere, 
+                function (err, rezUpdate){
+                    if(err || rezUpdate.rowCount==0){
+                        console.log("Cod:", err);
+                        afisareEroare(res,3);
+                    }
+                    else{
+                        res.render("pagini/confirmare.ejs");
+                    }
+                })
+        })
+    }
+    catch (e){
+        console.log(e);
+        afisareEroare(res,2);
+    }
+})
+
+app.get("/produse", function (req, res) {
+  console.log(req.query);
+  var conditieQuery = "";
+  if (req.query.tip) {
+    conditieQuery = ` where brand='${req.query.tip}'`;
+  }
+  client.query(
+    "select * from unnest(enum_range(null::categorie_brand))",
+    function (err, rezOptiuni) {
+      client.query(
+        `select * from produse ${conditieQuery}`,
+        function (err, rez) {
+          if (err) {
+            console.log(err);
+            afisareEroare(res, 2);
+          } else {
+            res.render("pagini/produse", {
+              produse: rez.rows,
+              optiuni: rezOptiuni.rows,
+            });
+          }
+        }
+      );
+    }
+  );
+});
+
+app.get("/produs/:id", function (req, res) {
+  client.query(
+    "select * from unnest(enum_range(null::categorie_brand))",
+    function (err, rezOptiuni) {
+      client.query(
+        `select * from produse where id=${req.params.id}`,
+        function (err, rez) {
+          if (err) {
+            console.log(err);
+            afisareEroare(res, 2);
+          } else {
+            res.render("pagini/produs", {
+              prod: rez.rows[0],
+              optiuni: rezOptiuni.rows,
+            });
+          }
+        }
+      );
+    }
+  );
+});
+
+app.get(["/", "/home", "/index"], function (req, res) {
+  client.query(
+    "select * from unnest(enum_range(null::categorie_brand))",
+    function (err, rezOptiuni) {
+      generateSmallerImages(galerie.imagini, galerie.cale_galerie);
+      const galerie_carousel = galerie.imagini;
+      const galerie_path = galerie.cale_galerie;
+      res.render("pagini/index", {
+        galerie_path: galerie_path,
+        galerie_carousel: galerie_carousel,
+        optiuni: rezOptiuni.rows,
+      });
+    }
+  );
+});
+
+app.get("/despre", function (req, res) {
+  client.query(
+    "select * from unnest(enum_range(null::categorie_brand))",
+    function (err, rezOptiuni) {
+      generateSmallerImages(galerie.imagini, galerie.cale_galerie);
+      const userIp = req.ip;
+      const galerie_filtrata = renderImagesBySeason(res);
+      const galerie_path = galerie.cale_galerie;
+      console.log("IMAGINI", galerie);
+      console.log("CALE", galerie_path, "\n\n\n\n");
+      res.render("pagini/despre", {
+        userIp: userIp,
+        galerie: galerie_filtrata,
+        galerie_path: galerie_path,
+        optiuni: rezOptiuni.rows,
+      });
+    }
+  );
+});
+
+app.get("/favicon.ico", function (req, res) {
+  const faviconPath = path.join(__dirname, "/resurse/favicon/favicon.ico");
+  res.sendFile(faviconPath);
+});
+
+app.get("/logout", function (req, res) {
+  req.session.destroy();
+  res.locals.utilizator = null;
+  res.render("pagini/logout");
+});
+
+app.get("/*", function (req, res) {
+  console.log(req.url);
+  res.render("pagini" + req.url, function (err, rezHtml) {
+    if (err) {
+      if (err.message.indexOf("Failed to lookup view") !== -1) {
+        const errorCode = parseInt(req.url.split("/")[1]);
+        const { obErori } = obGlobal;
+        const eroareData =
+          obErori.info_erori.find(
+            (eroare) => eroare.identificator === errorCode
+          ) || obErori.eroare_default;
+
+        afisareEroare(
+          res,
+          errorCode,
+          eroareData.titlu,
+          eroareData.text,
+          eroareData.imagine
+        );
+      } else {
+        throw err;
+      }
+    }
+    //console.log(rezHtml);
+    //console.log("Eroare: ", err);
+    res.send(rezHtml + "");
+  });
 });
 
 app.listen(8080);
